c77adbef4255212f8ac15a2759702fc2495c90b9
Ayca Bas <aycabas@microsoft.com>
2020-06-10 21:32:52 +0400

    Privacy tab removed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,10 +23,7 @@ namespace GraphToolkitNetCore.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
